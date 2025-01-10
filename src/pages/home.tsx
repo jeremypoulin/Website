87c8a9d7359fbc1ab0bdb6d372bcf5367e0b0f8c
Jeremy Poulin <150217207+jeremypoulin@users.noreply.github.com>
2025-01-10 04:37:09 -0500

    adjusted homepage for small screens

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TypeWriter from "../components/Typewriter";
 import RButton from "../components/RButton";
+import "./home.css";
 
 const Home = () => {
     return (<div className="centre-container">
