4b0397b255386898b67500dd9e7f094b82e68a6c
Jeremy Poulin <150217207+jeremypoulin@users.noreply.github.com>
2025-01-11 08:41:02 -0500

    form feedback and colour change

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -16,8 +16,7 @@ const Contact = () => {
             delaySpeed={1000}
           />
         </h1>
-        <h2 style={{textAlign: "center", marginBottom: "1px"}}>links</h2>
-        <p style={{textAlign: "center", marginTop: "1px"}}><a href="https://github.com/jeremypoulin" style={{color: "rgba(112, 0, 65, 0.87)"}}>GitHub</a><br></br>
+        <p style={{textAlign: "center", marginTop: "1px"}}><a href="https://github.com/jeremypoulin" style={{color: "rgb(112, 0, 65)"}}>GitHub</a><br></br>
         <a href="https://www.linkedin.com/in/jeremy-poulin-725938278" style={{color: "rgb(112, 0, 65)"}}>LinkedIn</a></p>
         <Form/>
         </div>
