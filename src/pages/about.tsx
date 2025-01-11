c8140d46acd6cc8d0e2b8652f3d8a3956be1a082
Jeremy Poulin <150217207+jeremypoulin@users.noreply.github.com>
2025-01-11 08:47:08 -0500

    colour change

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -13,11 +13,11 @@ const About = () => {
             deleteSpeed={50}
             delaySpeed={1000}
           /></h1>
-            <h2 style={{ textAlign: "center"}}>general</h2>
+            <h2 style={{ textAlign: "center", color: "rgb(145, 0, 58)"}}>general</h2>
             <p style={{padding: "30px", color: "lightgray"}}>Born and raised in Edmonton, Alberta, I moved across Canada to attend the University of Waterloo's reknowned Computer Engineering program. 
                 Alongside my studies, I have a variety of interests which I pursue daily.
             </p>
-            <h2 style={{ textAlign: "center" }}>tech</h2>
+            <h2 style={{ textAlign: "center", color: "rgb(145, 0, 58)" }}>tech</h2>
             <p style={{padding: "30px", color: "lightgray" }}>Since I was first introduced to the world of computers as a young child marvelling at the magic black boxes that were called cell phones, 
                 my passion for computer technology has grown exponentially. <br></br>
                 Upon the discovery of FPV drones, I immediately became obsessed with the world of UAVs (Unmanned Aerial Vehicles). 
@@ -26,13 +26,13 @@ const About = () => {
                 My initial encounter with programming was in 2022, when I discovered a music composition tool named Sonic Pi. 
                 Tinkering with this unique application piqued my interest in the world of software development. This newfound interest formed a base for what you see in the "projects" section.
                 </p>
-            <h2 style={{ textAlign: "center" }}>sports</h2>
+            <h2 style={{ textAlign: "center", color: "rgb(145, 0, 58)" }}>sports</h2>
             <p style={{padding: "30px", color: "lightgray" }}>As a lifelong athlete, sport has always played a significant role in my everyday life. Though training is as fun as a hobby to me, 
                 my athletic endeavours have opened unprecedented doors. In 2022 and 2023, I represented Canada at the Junior Marathon World Championships in the sport of Canoe/Kayak. <br></br>
                 Some of my earlier accomplishments include winning the Alberta Summer Games Individual Triathlon event, and competing at the Canada Summer Games for Sprint Canoe. <br></br>
                 Currently, I compete for the University of Waterloo's Varsity Nordic Ski Team.
             </p>
-            <h2 style={{ textAlign: "center" }}>music</h2>
+            <h2 style={{ textAlign: "center", color: "rgb(145, 0, 58)" }}>music</h2>
             <p style={{padding: "30px", color: "lightgray" }}>Music was my first love. For as long as I can remember, I have been playing and performing music, whether it be the piano, cello, or trombone.
                 For the past 5 years, I have also been recording music, and working with DAWs (Digital Audio Workstations). I aspire to continue to progress in music,
                 and plan on pursuing composition as a hobby.
