5bdcc545ae4036dea61c26bfecac0321b1adaf04
Jeremy Poulin <150217207+jeremypoulin@users.noreply.github.com>
2025-01-12 00:17:49 -0500

    updated resume

--- src/components/RButton.tsx
+++ src/components/RButton.tsx
@@ -17,7 +17,7 @@ const RButton: React.FC = () => {
     <div style={{ marginTop: "20px", marginLeft: "20px", display: "flex" }}>
     <button style={{ borderColor: "rgb(145, 0, 58)", background: "#000", borderWidth: "2px", display: "flex", justifyContent: "center", alignItems: "center", padding: "10px 20px", fontSize: "16px" }}>
       <a
-        href="/JeremyPoulinResumeV9.pdf"
+        href="/JeremyPoulinResumeV10.pdf"
         download="JeremyPoulinResume.pdf"
         style={{color: "rgb(145, 0, 58)", fontSize: "20px"}}>
         resume
