e639805980f2cbbc68aa393d4ededd79bbca8087
Jeremy Poulin <150217207+jeremypoulin@users.noreply.github.com>
2025-01-16 14:37:05 -0500

    smoother animations

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -11,7 +11,7 @@ const Home: React.FC = () => {
     useEffect(() => {
         const timer = setTimeout(() => {
           setShowTypewriter(true);
-        }, 500);
+        }, 1000);
     
         return () => clearTimeout(timer);
       }, []);
