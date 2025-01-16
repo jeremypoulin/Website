1cc94c667702aa28a963588fc4a5fb25794547b7
Jeremy Poulin <150217207+jeremypoulin@users.noreply.github.com>
2025-01-16 03:27:00 -0500

    added intro animation

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,20 +2,24 @@ import React from "react";
 import TypeWriter from "../components/Typewriter";
 import RButton from "../components/RButton";
 import "./home.css";
+import Pixel from "../components/pixel"
 
-const Home = () => {
-    return (<div className="centre-container">
+const Home: React.FC = () => {
+    const row = 20;
+    const col = 20;
+    return (<div className="home-container">
         <RButton></RButton>
-        <h1 style={{ display: "flex",justifyContent: "center", height: "100vh", marginTop: "250px", marginLeft: "20px", fontSize: "125px"}}>
-        <TypeWriter
+        <div className="centre-container">
+        <h1 style={{ position: "absolute", display: "flex",justifyContent: "center", height: "100vh", marginTop: "250px", marginLeft: "20px", fontSize: "125px"}}><TypeWriter
             words={["hi!", "my name is jeremy"]}
             cursor
             cursorStyle
             typeSpeed={70}
             deleteSpeed={50}
             delaySpeed={1000}
-          />
-    </h1>
+          /></h1>
+        <Pixel/>
+    </div>
     </div>)
 }
 
